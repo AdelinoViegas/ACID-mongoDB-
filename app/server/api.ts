3507af648a4d9212abc3ea016a50d553aa46e2ac
AdelinoViegas <buchoteclas@gmail.com>
2026-05-11 18:01:40 +0100

    actualiza a api

--- app/server/api.ts
+++ app/server/api.ts
@@ -3,50 +3,53 @@
 import mongoose from "mongoose";
 
 async function connectDB() {
-  if (mongoose.connection.readyState >= 1) return;
-  await mongoose.connect("mongodb://localhost:27017/test2");
+  //mongoose.set("transactionAsyncLocalStorage", true);
+  
+  if (mongoose.connection.readyState >= 1) 
+    return;
+
+  await mongoose.connect(
+    "mongodb://mongo:hello@localhost:27017,localhost:27018,localhost:27019/?replicaSet=rs0&authSource=admin"
+  );
 }
 
 const userschema = new mongoose.Schema({
-    name: String,
-    gender: String,
-    contact: String,
-    bi: String
+  name: String,
+  gender: String,
+  contact: String,
+  bi: String
 })
 
-const userModel = mongoose.model("User", userschema);
+const userModel = mongoose.models.User || mongoose.model("User", userschema);
 
 export async function sign(prev:unknown, form: FormData){
-  try{
-    await connectDB();
-    const session = await mongoose.startSession();
+  await connectDB();
+  const session = await mongoose.startSession();
 
+  try{
     const user = {
       name: form.get("name") as string,
       gender: form.get("gender") as string,
       contact: form.get("contact") as string,
       bi: form.get("bi") as string
     }
-
-    await session.withTransaction( async () =>{
-      await userModel.create(
-        [user],
-        {session}
-      );
-    });
     
-    await session.endSession();
+    await session.withTransaction( async () => {
+      await userModel.create([user], {session});
+    });
 
     return {
-        message: "Cadastro realizado com sucesso",
-        status: true
+      message: "cadastrado com sucesso!",
+      status: true
     }
+
  }catch(err: unknown){
     const error = err as Error;
 
+    console.log(error.message);
     return {
         message: error.cause?error.cause:"Erro ao cadastrar",
-        statu: false
+        status: false
     }
  }
 }
